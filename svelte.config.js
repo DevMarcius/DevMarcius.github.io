b6a6b51a5bad105c6705379f8e9a175d6d9bb506
DevMarcius <Marcius922@gmail.com>
2022-07-01 09:53:10 +0300

    1.0.1

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,15 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(
-			{fallback: '200.html'}
-		),
+		adapter: adapter({
+			target: '#svelte',
+			fallback: null,
+			prerender: {
+				default: true
+			},
+			pages: 'build',
+			assets: 'build',
+		}),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
